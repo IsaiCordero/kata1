e023085ec79746d4610d850a444938b8e8b4fee8
isai <isaicordero03@gmail.com>
2024-09-16 20:09:19 +0100

    frist version

---
+++ src/main/java/software/ulpgc/Main.java
@@ -0,0 +1,10 @@
+package software.ulpgc;
+
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Person("YO", LocalDate.of(2002, 8, 25)));
+    }
+}
